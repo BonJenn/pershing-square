5eefbe76a00508d459bdf60ce838d03fc1d86290
Jonathan Benn <bonjennprojects@gmail.com>
2024-07-11 12:37:55 -0700

    Header now animates up when the user scrolls down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,34 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from '../app/styles/Header.module.css';
 import Navigation from './Navigation.jsx';
 
 export default function Header({ setCurrentPage, currentPage }) {
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY && window.scrollY > 50) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isHidden ? styles.hidden : ''}`}>
       <div className={styles.logo}>
         <Image 
           src="/images/pershing_square_logo.png" 
