80acd11aa2c4df285026d7e235803ac50e3347fc
Jonathan Benn <bonjennprojects@gmail.com>
2024-07-11 19:00:40 -0700

    Added additional upcoming events

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -64,7 +64,7 @@ export default function Home() {
               className="main-hero" 
               videoSrc="/images/Pershing_Square_Video.mov"
               text="Summer Concert Series 2024"
-              subtext="The Hottest Concert Series is Back in DTLA"
+              subtext="The Hottest Concert Series in Los Angeles"
               buttonText="Schedule and Free Tickets"
               buttonLink="https://www.eventbrite.com/cc/los-angeles-free-concert-series-2332709"
             />
@@ -74,10 +74,14 @@ export default function Home() {
                 {sliderLoaded && (
                   <Slider {...settings} className={styles.customSlider}>
                     <EventSlide text="Forever 80s Live" subtext="Saturday, July 13th @ 3PM" imageSrc="/images/ps_event_7_13.jpeg" link="https://www.eventbrite.com/e/80s-forever-w-asia-when-in-rome-naked-eyes-many-more-free-event-tickets-936838668677?aff=odcleoeventsincollection" />
-                    <EventSlide text="MC Magic, Baby Bash, Lil Rob & Amanda Perez" subtext="Sunday, July 14th @ 3PM" imageSrc="/images/ps_event_7_14.jpeg" />
-                    <EventSlide text="Montel Jordan, Shai, Ruff Endz, + Blaque" subtext="Sunday, July 21 @ 3PM" imageSrc="/images/ps_event_7_21.jpeg" />
-                    <EventSlide text="Banda Limon, Banda MaGuey, y Mas!" subtext="Sunday, August 4th @ 3PM" imageSrc="/images/ps_event_8_4.jpeg" />
-                    <EventSlide text="Pop 2000 Tour" subtext="Sunday, August 11th @ 3PM" imageSrc="/images/ps_event_8_11.jpeg" />
+                    <EventSlide text="MC Magic, Baby Bash, Lil Rob & Amanda Perez" subtext="Sunday, July 14th @ 3PM" imageSrc="/images/ps_event_7_14.jpeg" link="https://www.eventbrite.com/e/free-event-mc-magic-baby-bash-lil-rob-amanda-perez-tickets-933811173357?aff=ebdssbdestsearch" />
+                    <EventSlide text="Golden Oldies Concert" subtext="Saturday, July 20th @ 3PM" imageSrc="/images/ps_event_7_20.jpeg" link="https://www.eventbrite.com/e/free-oldies-concert-w-the-stylistics-the-drifters-the-persuaders-more-tickets-945317980527?aff=erelpanelorg" />
+
+                    <EventSlide text="Montel Jordan, Shai, Ruff Endz, + Blaque" subtext="Sunday, July 21 @ 3PM" imageSrc="/images/ps_event_7_21.jpeg" link="https://www.eventbrite.com/e/free-rnb-concert-with-montell-jordan-shai-ruff-endz-blaque-more-tickets-944593282937?aff=ebdssbdestsearch" />
+                    <EventSlide text="Banda Limon, Banda MaGuey, y Mas!" subtext="Sunday, August 4th @ 3PM" imageSrc="/images/ps_event_8_4.jpeg" link="https://www.eventbrite.com/e/free-banda-limon-banda-maguey-y-mas-gratis-tickets-942917440447?aff=ebdssbdestsearch" />
+                    <EventSlide text="Pop 2000 Tour" subtext="Sunday, August 11th @ 3PM" imageSrc="/images/ps_event_8_11.jpeg" link="https://www.eventbrite.com/e/free-concert-w-nsyncs-chris-kirkpatrick-o-town-the-calling-lfo-pop-tour-tickets-934920230577?aff=ebdssbdestsearch" />
+                    <EventSlide text="Bachatavibez Fest" subtext="Saturday, August 24th @ 3PM" imageSrc="/images/ps_event_8_24.jpeg" link="https://www.eventbrite.com/e/bachatavibez-fest-tickets-787881664267?aff=ebdssbdestsearch" />
+
                   </Slider>
                 )}
               </div>
