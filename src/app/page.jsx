19aa3c74cdd419974f9803176414679dcd3dc48b
Jonathan Benn <bonjennprojects@gmail.com>
2024-07-15 12:20:54 -0700

    Updated lineup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,8 +12,6 @@ import EventsList from '../components/EventsList';
 import styles from './styles/Home.module.css';
 
 const eventsData = [
-  { id: 1, date: '2024-07-13', text: 'Forever 80s Live', subtext: 'Saturday, July 13th @ 3PM', imageSrc: '/images/ps_event_7_13.jpeg', link: 'https://www.eventbrite.com/e/80s-forever-w-asia-when-in-rome-naked-eyes-many-more-free-event-tickets-936838668677?aff=odcleoeventsincollection' },
-  { id: 2, date: '2024-07-14', text: 'MC Magic, Baby Bash, Lil Rob & Amanda Perez', subtext: 'Sunday, July 14th @ 3PM', imageSrc: '/images/ps_event_7_14.jpeg', link: 'https://www.eventbrite.com/e/free-event-mc-magic-baby-bash-lil-rob-amanda-perez-tickets-933811173357?aff=ebdssbdestsearch' },
   { id: 3, date: '2024-07-20', text: 'Golden Oldies Concert', subtext: 'Saturday, July 20th @ 3PM', imageSrc: '/images/ps_event_7_20.jpeg', link: 'https://www.eventbrite.com/e/free-oldies-concert-w-the-stylistics-the-drifters-the-persuaders-more-tickets-945317980527?aff=erelpanelorg' },
   { id: 4, date: '2024-07-21', text: 'Montel Jordan, Shai, Ruff Endz, + Blaque', subtext: 'Sunday, July 21 @ 3PM', imageSrc: '/images/ps_event_7_21.jpeg', link: 'https://www.eventbrite.com/e/free-rnb-concert-with-montell-jordan-shai-ruff-endz-blaque-more-tickets-944593282937?aff=ebdssbdestsearch' },
   { id: 5, date: '2024-08-04', text: 'Banda Limon, Banda MaGuey, y Mas!', subtext: 'Sunday, August 4th @ 3PM', imageSrc: '/images/ps_event_8_4.jpeg', link: 'https://www.eventbrite.com/e/free-banda-limon-banda-maguey-y-mas-gratis-tickets-942917440447?aff=ebdssbdestsearch' },
@@ -86,13 +84,15 @@ export default function Home() {
                 <h2>Upcoming Events</h2>
                 {sliderLoaded && (
                   <Slider {...settings} className={styles.customSlider}>
-                    <EventSlide text="Forever 80s Live" subtext="Saturday, July 13th @ 3PM" imageSrc="/images/ps_event_7_13.jpeg" link="https://www.eventbrite.com/e/80s-forever-w-asia-when-in-rome-naked-eyes-many-more-free-event-tickets-936838668677?aff=odcleoeventsincollection" />
-                    <EventSlide text="MC Magic, Baby Bash, Lil Rob & Amanda Perez" subtext="Sunday, July 14th @ 3PM" imageSrc="/images/ps_event_7_14.jpeg" link="https://www.eventbrite.com/e/free-event-mc-magic-baby-bash-lil-rob-amanda-perez-tickets-933811173357?aff=ebdssbdestsearch" />
-                    <EventSlide text="Golden Oldies Concert" subtext="Saturday, July 20th @ 3PM" imageSrc="/images/ps_event_7_20.jpeg" link="https://www.eventbrite.com/e/free-oldies-concert-w-the-stylistics-the-drifters-the-persuaders-more-tickets-945317980527?aff=erelpanelorg" />
-                    <EventSlide text="Montel Jordan, Shai, Ruff Endz, + Blaque" subtext="Sunday, July 21 @ 3PM" imageSrc="/images/ps_event_7_21.jpeg" link="https://www.eventbrite.com/e/free-rnb-concert-with-montell-jordan-shai-ruff-endz-blaque-more-tickets-944593282937?aff=ebdssbdestsearch" />
-                    <EventSlide text="Banda Limon, Banda MaGuey, y Mas!" subtext="Sunday, August 4th @ 3PM" imageSrc="/images/ps_event_8_4.jpeg" link="https://www.eventbrite.com/e/free-banda-limon-banda-maguey-y-mas-gratis-tickets-942917440447?aff=ebdssbdestsearch" />
-                    <EventSlide text="Pop 2000 Tour" subtext="Sunday, August 11th @ 3PM" imageSrc="/images/ps_event_8_11.jpeg" link="https://www.eventbrite.com/e/free-concert-w-nsyncs-chris-kirkpatrick-o-town-the-calling-lfo-pop-tour-tickets-934920230577?aff=ebdssbdestsearch" />
-                    <EventSlide text="Bachatavibez Fest" subtext="Saturday, August 24th @ 3PM" imageSrc="/images/ps_event_8_24.jpeg" link="https://www.eventbrite.com/e/bachatavibez-fest-tickets-787881664267?aff=ebdssbdestsearch" />
+                    {eventsData.map(event => (
+                      <EventSlide 
+                        key={event.id}
+                        text={event.text}
+                        subtext={event.subtext}
+                        imageSrc={event.imageSrc}
+                        link={event.link}
+                      />
+                    ))}
                   </Slider>
                 )}
               </div>
